c6d931ee5423e2bb85edf55c37f2ccfc6b22c194
TaeHwanKin <haja019@naver.com>
2024-07-15 11:30:12 +0900

    수정

---
+++ JS/ch01.개요/02.HelloWorld.js
@@ -0,0 +1,11 @@
+// 자바 스크립트의 주석(comment)
+console.log('Hello World!!!')
+
+/*
+ * 반복문 - 여러줄에 걸쳐서 주석을 작성하는 경우
+ */
+let sum = 0;
+for (let i = 1; 1 <= 1000; i++) {
+    sum += i;
+}
+console.log('1에서 1000까지의 합은' , sum);
