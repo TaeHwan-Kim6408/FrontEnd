9729b0ccb788491c6c91bf4d08ae11d683070585
TaeHwanKin <haja019@naver.com>
2024-07-17 16:10:41 +0900

    수정

--- JS/ch04.반복문/08.nestedFor.js
+++ JS/ch04.반복문/08.nestedFor.js
@@ -1,12 +1,12 @@
 // 중첩(nested) for 반복문
 
-// 1. 별 다섯개를 다섯줄 그리기
-for (let i = 1; i < 6; i++) {
-    let Line = ' ';
-    for (let k = 0; k < 5; k++){
-        Line += '* ';
+// 1. 별 다섯개를 여섯줄 그리기
+for (let i = 0; i < 6; i++) {
+    let line = '';
+    for (let k = 0; k < 5; k++) {
+        line += '* ';
     }
-    console.log(Line);
+    console.log(line);
 }
 
 // 2. 디지털 시계를 매 10분마다 표시하기
@@ -18,29 +18,28 @@ for (let hour = 0; hour <= 23; hour++) {
 }
 
 // 3. 별을 대각선 모양으로 그리기 (다이아몬드 1사분면)
-for (let i = 0; i < 4; i++) {
-    let Line = ' ';
-    for (let k = 0; k < i + 1; k++)
-        Line += '* '
-    console.log(Line);
+for (let i = 0; i < 5; i++) {
+    let line = '';
+    for (let k = 0; k < i + 1; k++) 
+        line += '* '
+    console.log(line);
 }
-
 
 // 4. 다이아몬드 4사분면
 for (let i = 5; i > 0; i--) {
-    let Line = '';
+    let line = '';
     for (let k = 0; k < i; k++)
-        Line += '* ';
-    console.log(Line);
+        line += '* ';
+    console.log(line);
 }
 
 // 5. 주사위 눈으로 6 x 6 배열 만들기
 let diceArray = [];
 for (let i = 0; i < 6; i++) {
-    let Line = [];
+    let line = [];
     for (let k = 0; k < 6; k++) {
-        Line.push(Math.random() * 6);
+        line.push(Math.ceil(Math.random() * 6));
     }
-    diceArray.push(Line);
+    diceArray.push(line);
 }
 console.log(diceArray);
--- JS/ch04.반복문/09.구구단.js
+++ JS/ch04.반복문/09.구구단.js
@@ -1,27 +1,26 @@
 // 구구단
-
 for (let i = 2; i <= 9; i++) {
-    for (let k = 1; k <= 9; k++){
+    for (let k = 1; k <= 9; k++) {
         console.log(`${i} x ${k} = ${i*k}`);
     }
+    console.log();
 }
-
 
 // 다이아몬드 그리기
-let N = 4;                                                  // 가운데 두꺼운 별이 9개
+let N = 4;      // 가운데 두꺼운 별이 9개
 for (let i = 0; i <= N; i++) {
-    let Line = '';
-    for (let k = 0; k < N - i; k++)
-        Line += ' ';
+    let line = '';
+    for (let k = 0; k < N - i; k++) 
+        line += ' ';
     for (let k = 0; k < 2 * i + 1; k++)
-        Line += '*';
-    console.log(Line);
+        line += '*';
+    console.log(line);
 }
 for (let i = N - 1; i >= 0; i--) {
-    Let Line = '';
-    for (let k = 0; k < N - i; k++)
-        Line += ' ';
+    let line = '';
+    for (let k = 0; k < N - i; k++) 
+        line += ' ';
     for (let k = 0; k < 2 * i + 1; k++)
-        Line += '*';
-    console.log(Line);
+        line += '*';
+    console.log(line);
 }
