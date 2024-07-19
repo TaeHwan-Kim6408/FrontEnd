d5727e84a0aa94d5976c4044bf27cc1a8be4be6c
TaeHwanKin <haja019@naver.com>
2024-07-19 09:05:08 +0900

    수정

--- JS/ch04.반복문/07.약수.js
+++ JS/ch04.반복문/07.약수.js
@@ -1,5 +1,4 @@
 // 약수 구하기
-
 let num = 12;
 let divisors = [];
 for (let i = 1; i <= num; i++) {
@@ -9,8 +8,7 @@ for (let i = 1; i <= num; i++) {
 console.log(divisors);
 
 // 소수(prime number) 판별
-let numbers = [5, 11, 17, 23, 27];
-for (let i = 2; i < num; i++){
+for (let i = 2; i < num; i++) {
     if (num % i == 0) {
         console.log(`${num} 은/는 소수가 아님.`);
         break;
